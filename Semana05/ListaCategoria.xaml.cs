d67cf9fb0ec20e1e3bb32426637228ce65919618
JeremyNunez <48325661+JeremyNunez@users.noreply.github.com>
2020-04-27 22:50:20 -0500

    Add files via upload

---
+++ Semana05/ListaCategoria.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Business;
+using Entity;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        public ListaCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(Object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        private void Cargar()
+        {
+            BCategoria Bcategoria = null;
+            try
+            {
+                Bcategoria = new BCategoria();
+                dgvCategoria.ItemsSource = Bcategoria.Listar(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            ManCategoria manCategoria = new ManCategoria(0);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvCategoria_SelectionChanged(Object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)dgvCategoria.SelectedItems;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+
+            ManCategoria manCategoria = new ManCategoria(idCategoria);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+    }
+}
